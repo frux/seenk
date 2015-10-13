c1ddd3f74004d7db8106a8373a0772280fd5f518
Vladimir Kudinov <frux@yandex-team.ru>
2015-10-13 12:10:52 +0500

    New iteration model

--- index.js
+++ index.js
@@ -4,74 +4,28 @@
  * @version 0.1.0
  */
 
-//save original .next() method
-var _originalNext = (function*(){})().constructor.prototype.next,
-
-    //construct new .next() method
-    _next = function(iterable, yieldData){
-
-        //call original method
-        var yieldResult = _originalNext.call(iterable, yieldData),
-            yieldValue;
-
-        //if generator is done
-        if(yieldResult.done){
-
-            //just return result
-            return yieldResult;
-        }
-
-        //if value fo yield is Promise
-        if(!(yieldResult.value instanceof Promise)){
-
-            //let it be a yield value
-            yieldValue = yieldResult.value;
-
-            //if value is not a promise
-        }else{
-
-            //wrap it by a promise
-            yieldValue = new Promise(function(resolve, reject){
-                resolve(yieldResult.value);
-            });
-        }
-
-        //return patched yield result
-        return {
-            done: false,
-            value: yieldValue
-        };
-    };
-
 /**
- * Iterates yields
- * @param iterable {Generator} Generstor object
- * @param yieldValue {*} Value of previous yield
- * @returns {Promise}
+ *
+ * @param iterator {Generator} Generator
+ * @param resolve {function} Function resolves promise
+ * @param prevResult {*} Result of previous iteration
  * @private
  */
-function _yieldIterator(iterable, yieldValue){
+function _yieldIterator(iterator, resolve, prevResult){
+    var yieldResult = iterator.next(prevResult);
 
-    //return a promise
-    return new Promise(function(resolve, reject){
+    if(yieldResult.done){
+        resolve(yieldResult.value);
+        return;
+    }
 
-        //if result of yield is a promise
-        if(yieldValue instanceof Promise){
-
-            //then promise resolves
-            yieldValue.then(function(nextYieldValue){
-
-                //run new iteration
-                resolve(_yieldIterator(iterable, _next(iterable, nextYieldValue).value));
+    if(yieldResult.value instanceof Promise){
+            yieldResult.value.then(function(nextResult){
+                _yieldIterator(iterator, resolve, nextResult);
             });
-
-            //if yield returns not a promise
-        }else{
-
-            //resolve this value;
-            resolve(yieldValue);
-        }
-    });
+    }else{
+        _yieldIterator(iterator, resolve, nextResult);
+    }
 }
 
 /**
@@ -85,8 +39,10 @@ function _synchronize(generator){
     //patch generator
     var iterable = generator();
 
-    //run iterator
-    return _yieldIterator(iterable, iterable.next().value);
+    //run iterator and return promise
+    return new Promise(function(resolve, reject){
+        _yieldIterator(iterable, resolve);
+    });
 }
 
 /**
